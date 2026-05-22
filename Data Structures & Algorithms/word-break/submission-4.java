093cde3306d0cce51a0e65023228c93295de8e33
Vedant G <notification.aitc@gmail.com>
2026-05-22 14:37:10 +0530

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> dict = new HashSet<>(wordDict);
+        int n = s.length();
+        char[] sarr = s.toCharArray();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (dp[j] == true) {
+                    String curr = s.substring(j, i + 1);
+                    if (dict.contains(curr)) {
+                        dp[i + 1] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[n];
+    }
+}
